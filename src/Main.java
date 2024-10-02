a08167c28b8b42bd9e862b942c8687c487115bee
Ytalo <ytalo986.x@gmail.com>
2024-10-02 08:58:46 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+// Meu Primeiro codigo no java
+public class Main {
+    public static void main(String[] args) {
+        byte[] VALORES = {10,20,50};
+        boolean[] VERDADEIROS = {true,false,true};{
+            // Inicio do sistema de notas verdadeiras (pix)
+            System.out.println("| Notas Verdadeiras |");
+            System.out.println("------------------");
+            // Constantes que reprsentam valores fixos
+            System.out.println(VALORES[0]+"$   "+VALORES[1]+"$   "+VALORES[2]+"$");
+            // Constantes que representam true e false fixos.
+            System.out.println(VERDADEIROS[0]+" "+VERDADEIROS[1]+" "+VERDADEIROS[2]);
+
+        }
+        String[] Nomes = {"Ytalo","Tutu","Lehi"};{
+            System.out.println(Nomes[0]+" "+Nomes[1]+" "+Nomes[2]);
+            System.out.println("-------------------");
+        }
+
+
+
+
+
+
+
+    }
+}
